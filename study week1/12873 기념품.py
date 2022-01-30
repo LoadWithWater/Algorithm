f7e4bc10024f82fdd95b64e44e6b027a0c234490
Kim <rlarhkdwo98@gmail.com>
2022-01-30 22:46:41 +0900

    add sw1 sw2

---
+++ study week1/12873 기념품.py
@@ -0,0 +1,20 @@
+import math
+
+N = int(input()) #사람 수 입력 받기
+
+Gamers = list(range(1, N+1))
+
+i = 0
+GameCount = N-1
+while True:
+  if i == GameCount:
+    break
+  else:
+    i += 1
+
+  mul = int(math.pow(i, 3))
+  Del = mul % len(Gamers)
+  Del -= 1
+  Gamers.pop(Del)
+
+print(Gamers[0])
